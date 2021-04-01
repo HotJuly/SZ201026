ed7e288774203549dcfbefe179af4e78002b855d
HotJuly <949588380@qq.com>
2021-04-01 15:33:47 +0800

    18.用户二次登陆免授权功能

--- 1.微信小程序/02.代码/wechat-init/pages/index/index.js
+++ 1.微信小程序/02.代码/wechat-init/pages/index/index.js
@@ -4,7 +4,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    msg:"花花"
+    msg:"花花",
+    // 用户基本信息
+    userInfo:{}
   },
   // 面试题:如何实现对象深拷贝
   //  JSON.parse(JSON.stringify(obj))乞丐版
@@ -18,6 +20,18 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
+  getUserInfo(res){
+    // 情况是一个回调函数,需要接受用户信息
+    // 第一种:形参  ->  res.detail.userInfo
+    // 第二种:this
+    console.log('getUserInfo', res)
+    // 判断用户是否允许获取信息res.detail
+    if(res.detail.rawData){
+      this.setData({
+        userInfo: res.detail.userInfo
+      })
+    }
+  },
   handleClick(){
     this.setData({
       msg:"我是修改之后的数据"
@@ -40,6 +54,21 @@ Page({
     //   url: "../logs/logs"
     // })
   },
+  getUserProfile(){
+
+    console.log(wx.canIUse("getUserProfile"))
+    wx.getUserProfile({
+      desc: '用于完善会员资料',
+      success: (detail) => {
+        console.log('success', detail);
+        this.setData({
+          userInfo: detail.userInfo
+        })
+      }
+    })
+
+  },
+
   onLoad: function (options) {
     // console.log('0',this.data.msg)
     // this.setData({
@@ -50,6 +79,17 @@ Page({
     //     msg:4
     // })
     // console.log('4', this.data.msg)
+
+
+    // 使用wx.getUserInfo获取用户信息
+    // wx.getUserInfo({
+    //   success:(detail)=>{
+    //     // console.log('success', res);
+    //     this.setData({
+    //       userInfo: detail.userInfo
+    //     })
+    //   }
+    // })
 
   },
 
